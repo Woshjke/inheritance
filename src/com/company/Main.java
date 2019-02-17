1fc833437843fb7e4790c65ee972c438902092d3
User <brillliant@tut.by>
2019-02-17 21:37:23 +0300

    first
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         DataProvider dataProvider = new DataProvider();
-
         Account painter = dataProvider.getPainter();
-        Account seller = dataProvider.getSeller();
-        Account client = dataProvider.getClient();
 
         System.out.println("painter tax for 4 quarter 2018: " + painter.getTax(4, 2018));
 
