41d46313294373c578c41371d05992319f8cdfd5
Yauheni.Pashkelevich <Y.Pashkelevich@mps-solutions.de>
2019-04-08 15:50:24 +0300

    task2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,7 @@ public class Main {
         AccountIE company = new AccountIE();
         Account ieSimple = dataProvider.getIeSimple();
         ieSimple.setPayments(dataProvider.generateIESimplePayments());
+        ieSimple.setInPreferentialTaxZone(true);
 
         System.out.println("painter tax for 4 quarter 2018: " + painter.getTax(4, 2018));
         System.out.println("Printing fixed tax for individual entrepreneur : " + company.getTax(1,2));
